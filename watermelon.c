081160796c089e9fbb23e2d9239df5eaa11ffadc
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-03-17 01:43:47 +0600

    Update watermelon.c

--- watermelon.c
+++ watermelon.c
@@ -1,13 +1,7 @@
 ## Problem 4A: Watermelon
 
-**Link to Problem**: [Codeforces Problem 4A](http://codeforces.com/contest/4/problem/A)
-222989274	Sep/11/2023 23:40UTC+6	eshat69	A - Watermelon	GNU C11	Accepted	30 ms	0 KB
-### Problem Statement:
-You are given a watermelon with a weight of `w` kilograms. You need to determine if it can be divided into two parts such that each part weighs an even number of kilograms.
-
-### Solution Explanation:
-We check if the weight is even and greater than 2. If both conditions are met, the answer is "YES". Otherwise, it's "NO".
-
+http://codeforces.com/contest/4/problem/A
+// 222989274	Sep/11/2023 23:40UTC+6	eshat69	A - Watermelon	GNU C11	Accepted	30 ms	0 KB
 ### Code (C):
 
 #include<stdio.h>
