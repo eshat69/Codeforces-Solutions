a5917bcca1e71ff76699e2fb03258d3f08fac202
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-03-18 01:26:23 +0600

    Update stones_on_the_table.c

--- stones_on_the_table.c
+++ stones_on_the_table.c
@@ -1,3 +1,9 @@
+## Problem A. Stones on the Table
+
+https://codeforces.com/contest/266/problem/A
+223332219	Sep/14/2023 16:43UTC+6	eshat69	A - Stones on the Table	GNU C11	Accepted	30 ms	0 KB
+### Code (C):
+
 #include <stdio.h>
 int main() {
     int n, i, count = 0;
