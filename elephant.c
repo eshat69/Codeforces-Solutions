aed7debda587f606385907852201ddd5dc18a1d1
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-03-18 01:22:46 +0600

    Update elephant.c

--- elephant.c
+++ elephant.c
@@ -1,3 +1,8 @@
+## Problem A. Elephant
+
+https://codeforces.com/contest/617/problem/A
+223049656	Sep/12/2023 13:37UTC+6	eshat69	A - Way Too Long Words	GNU C11	Accepted	15 ms	0 KB
+### Code (C):
 #include<stdio.h>
 int main (){
     int n , s ;
