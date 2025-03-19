0a1a346a979f717f90d24abf9ea92c13434f698d
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-03-20 02:23:55 +0600

    Update tram,c.c

--- tram,c.c
+++ tram,c.c
@@ -1,3 +1,10 @@
+
+## Problem A. Tram
+
+https://codeforces.com/contest/116/problem/A
+// 224021213	Sep/19/2023 16:34UTC+6	eshat69	A - Tram	GNU C11	Accepted	62 ms	0 KB
+### Code (C):
+
 #include<stdio.h>
 int main () {
     int n,i,a,b,pass=0, max=0 ;
