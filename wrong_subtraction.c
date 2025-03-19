7056d72dd216df569385ac18ade0bd5e5aae284e
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-03-20 02:09:33 +0600

    Update wrong_subtraction.c

--- wrong_subtraction.c
+++ wrong_subtraction.c
@@ -1,3 +1,8 @@
+##  problem: (A) Wrong Subtraction
+
+https://codeforces.com/contest/977/problem/A
+// 223729624	Sep/17/2023 19:09UTC+6	eshat69	A - Wrong Subtraction	GNU C11	Accepted	15 ms	0 KB
+### Code (C):
 #include<stdio.h>
 int main (){
     int n,k,i ;
