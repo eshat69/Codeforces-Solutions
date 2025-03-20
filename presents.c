80e919f4022f91727dfe28f998fc70928bddfaba
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-03-21 00:07:32 +0600

    Add files via upload

---
+++ presents.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main (){
+    int n,i,temp ;
+    scanf("%d",&n);
+    int arr[n+1] ;
+    for(i=1 ; i<=n ; i++){
+        scanf("%d",&temp);
+        arr[temp]=i ;
+    }
+    for(i=1 ; i<=n ; i++){
+        printf("%d ",arr[i]);
+    }
+    return 0 ;
+}
