8da9f74ccfa6dc6279dafd7ee1f85146e98b4bac
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-03-23 01:21:31 +0600

    Update magnets.c

--- magnets.c
+++ magnets.c
@@ -1,3 +1,9 @@
+## Problem A. Magnets
+
+https://codeforces.com/contest/344/problem/A
+224292670	Sep/21/2023 18:17UTC+6	eshat69	A - Magnets	GNU C11	Accepted	62 ms	0 KB
+### Code (C):
+
 #include<stdio.h>
 #include<stdio.h>
 int main () {
