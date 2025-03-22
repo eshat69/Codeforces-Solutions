29d01d2ef9ac93aac3b20bde744a14473b4f18e7
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-03-23 01:26:04 +0600

    Update vanya_and_fence.c

--- vanya_and_fence.c
+++ vanya_and_fence.c
@@ -1,3 +1,9 @@
+## Problem A. Vanya and Fence
+
+https://codeforces.com/contest/677/problem/A
+224151217	Sep/20/2023 16:54UTC+6	eshat69	A - Vanya and Fence	GNU C11	Accepted	15 ms	0 KB
+### Code (C):
+
 #include<stdio.h>
 int main (){
     int n,h,i,w=0,temp ;
