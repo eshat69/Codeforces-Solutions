ef52f1afd63d68dba2f8aee3b795afe3aa5939dd
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-03-23 01:53:13 +0600

    Update fox_and_snake.c

--- fox_and_snake.c
+++ fox_and_snake.c
@@ -1,16 +1,18 @@
-#include<stdio.h>
-int main() {
-    int n, m, i, j;
-    scanf("%d %d", &n, &m);
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < m; j++) {
-            if (i % 2 == 0 || (i % 4 == 1 && j == 0) || (i % 4 == 3 && j == m - 1)) {
-                printf("#");
-            } else {
-                printf(".");
-            }
-        }
-        printf("\n");
-    }
-    return 0;
+## Problem A. Fox And Snake
+
+https://codeforces.com/contest/510/problem/A
+225895670	Sep/30/2023 17:12UTC+6	eshat69	A - Fox And Snake	GNU C11	Accepted	31 ms	0 KB
+### Code (C):
+
+#include <stdio.h>
+int r,c,i,j;
+int main(){
+	scanf("%d%d",&r,&c);
+	for(i=0;i<r;i++){
+	 for(j=0;j<c;j++)
+	 	if(i%2==0 || (i%4==1&&j==c-1)||(j==0&&i%4==3))
+	 		printf("#");
+	 	else printf(".");
+	printf("\n");
+	}
 }
