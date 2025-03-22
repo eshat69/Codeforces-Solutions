579de7eb594abdf68c360ce12f841bc35b39a9ec
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-03-23 01:14:43 +0600

    Update divisibility_problem.c

--- divisibility_problem.c
+++ divisibility_problem.c
@@ -1,3 +1,8 @@
+## Problem A. Divisibility Problem
+
+https://codeforces.com/contest/1328/problem/A
+225501749	Sep/27/2023 17:31UTC+6	eshat69	A - Divisibility Problem	GNU C11	Accepted	46 ms	3900 KB
+### Code (C):
 #include <stdio.h>
 int main() {
     int n, a, b, result;
