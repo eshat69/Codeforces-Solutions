a28877079b10785a1fca93bbbabae9f34a054336
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-03-23 01:16:22 +0600

    Update calculating_function.c

--- calculating_function.c
+++ calculating_function.c
@@ -1,3 +1,8 @@
+## Problem A. Calculating Function
+
+http://codeforces.com/contest/486/problem/A
+224735725	Sep/23/2023 17:08UTC+6	eshat69	A - Calculating Function	GNU C11	Accepted	31 ms	0 KB
+### Code (C):
 #include<stdio.h>
 int main (){
     long long n , sum ;
