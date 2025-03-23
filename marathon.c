62530ba7b13034f40c3e7fc8f21708585b6fc758
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-03-24 00:26:47 +0600

    Add files via upload

---
+++ marathon.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int t, a, b, c, d;
+    scanf("%d", &t);
+    while (t--) {
+        scanf("%d %d %d %d", &a, &b, &c, &d);
+        int count = 0;
+        if (b > a) {
+            count++;
+        }
+        if (c > a) {
+            count++;
+        }
+        if (d > a) {
+            count++;
+        }
+        printf("%d\n", count);
+    }
+    return 0;
+}
