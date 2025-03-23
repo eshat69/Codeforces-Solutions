156785e08f8af1c2e291ae4ab94fb15fbf617b24
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-03-24 00:28:24 +0600

    Update marathon.c

--- marathon.c
+++ marathon.c
@@ -1,3 +1,9 @@
+## Problem A. Marathon
+
+https://codeforces.com/contest/1692/problem/A
+226562034	Oct/04/2023 15:13UTC+6	eshat69	A - Marathon	GNU C11	Accepted	46 ms	0 KB
+### Code (C):
+    
 #include <stdio.h>
 int main() {
     int t, a, b, c, d;
