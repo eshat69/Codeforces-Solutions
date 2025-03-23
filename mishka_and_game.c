ed7cacfc7314e32401f985dcd6fcbb846ee86d8e
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-03-24 00:20:23 +0600

    Update mishka_and_game.c

--- mishka_and_game.c
+++ mishka_and_game.c
@@ -1,3 +1,9 @@
+## Problem A. Mishka and Game
+
+https://codeforces.com/contest/703/problem/A
+226281011	Oct/02/2023 16:59UTC+6	eshat69	A - Mishka and Game	GNU C11	Accepted	30 ms	0 KB
+### Code (C):
+    
 #include <stdio.h>
 int main() {
     int n, m, c, s = 0;
