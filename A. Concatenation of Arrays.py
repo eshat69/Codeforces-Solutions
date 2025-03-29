9c0def68439eed9be8937494a465455f5e5c9fe9
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-03-30 00:58:09 +0600

    Update A. Concatenation of Arrays.py

--- A. Concatenation of Arrays.py
+++ A. Concatenation of Arrays.py
@@ -1,3 +1,9 @@
+## Problem A. Concatenation of Arrays
+
+https://codeforces.com/contest/2023/problem/A
+288804943	Oct/30/2024 01:52UTC+6	eshat69	A - Concatenation of Arrays	Python 3	Accepted	687 ms	31600 KB
+### Code (python):
+
 R=lambda:[*map(int,input().split())]
 for _ in[0]*R()[0]:
  for x in sorted((R()for _ in[0]*R()[0]),key=sorted):print(*x)
