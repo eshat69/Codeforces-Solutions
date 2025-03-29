3171d01a446a76e75df125c6d921ea78c2d28d21
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-03-30 01:15:17 +0600

    Update C. BA-String.py

--- C. BA-String.py
+++ C. BA-String.py
@@ -1,3 +1,8 @@
+## Problem C. BA-String
+
+https://codeforces.com/contest/1620/problem/C
+290557499	Nov/08/2024 20:49UTC+6	eshat69	C - BA-String	Python 3	Accepted	93 ms	7200 KB
+### Code (python):
 
 I=input
 for _ in[0]*int(I()):
