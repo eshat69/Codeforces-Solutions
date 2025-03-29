aa8fb71f1cd4d7488445e8aefd610e17800aa20b
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-03-30 00:27:13 +0600

    Update Holiday Of Equality.c

--- Holiday Of Equality.c
+++ Holiday Of Equality.c
@@ -1,17 +1,27 @@
+## Problem A - Holiday Of Equality
+
+https://codeforces.com/contest/758/problem/A
+227236843	Oct/09/2023 00:01UTC+6	eshat69	A - Holiday Of Equality	GNU C11	Accepted	15 ms	0 KB
+### Code (C):
+
+
 #include <stdio.h>
 int i, j, m, n, h;
 int main() {
+    
     scanf("%d", &n);
     m = 0;
     h = 0;
     for (i = 1; i <= n; i++) {
         scanf("%d", &j);
-        m += j;
+        m += j; // Accumulate the sum of 'j' in 'm'
         if (j > h) {
-            h = j;
+            h = j; // Update 'h' with the maximum value of 'j'
         }
     }
     printf("%d", n * h - m);
+
     return 0;
 }
+
 
