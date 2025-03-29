3f928a9dd5b28d727f283a18c51587c4e592cb2d
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-03-30 00:41:09 +0600

    Update Sakurako and Kosuke.py

--- Sakurako and Kosuke.py
+++ Sakurako and Kosuke.py
@@ -1,3 +1,10 @@
+## Problem A - Sakurako and Kosuke
+
+https://codeforces.com/contest/2033/problem/A
+287706345	Oct/24/2024 20:53UTC+6	eshat69	A - Sakurako and Kosuke	Python 2	Accepted	62 ms	0 KB
+### Code (python):
+
+
 t = int(input())
 for _ in range(t):
     n = int(input())
