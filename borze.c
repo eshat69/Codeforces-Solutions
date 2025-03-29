28ae8cfe47bd6a33c1fbba5ef59242defb7eb875
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-03-30 00:23:57 +0600

    Update borze.c

--- borze.c
+++ borze.c
@@ -1,3 +1,10 @@
+## Problem B. Borze
+
+https://codeforces.com/contest/32/problem/B
+227294083	Oct/09/2023 14:39UTC+6	eshat69	B - Borze	GNU C11	Accepted	30 ms	0 KB
+### Code (C):
+
+
 #include<stdio.h>
 char s[205];
 int main()
