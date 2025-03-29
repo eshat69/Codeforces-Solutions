e1599b2edc10679e708e6d019df7a0abc90aa0ee
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-03-30 00:56:02 +0600

    Update E. Sakurako, Kosuke, and the Permutation.py

--- E. Sakurako, Kosuke, and the Permutation.py
+++ E. Sakurako, Kosuke, and the Permutation.py
@@ -1,3 +1,10 @@
+## Problem E. Sakurako, Kosuke, and the Permutation
+
+https://codeforces.com/contest/2033/problem/E
+288804396	Oct/30/2024 01:48UTC+6	eshat69	E - Sakurako, Kosuke, and the Permutation	Python 3	Accepted	905 ms	125500 KB
+### Code (python):
+
+
 for s in[*open(0)][2::2]:
  a=[0,*map(int,s.split())];r=0
  for i in range(1,len(a)):
