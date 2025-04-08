33e38eaabef2c8c5204642d6b0248cca925c2050
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-04-09 00:20:19 +0600

    Update B. Friends and Presents.py

--- B. Friends and Presents.py
+++ B. Friends and Presents.py
@@ -1,3 +1,10 @@
+## Problem B - Friends and Presents
+
+https://codeforces.com/contest/483/problem/B
+291352867	Nov/13/2024 23:35UTC+6	eshat69	B - Friends and Presents	Python 3	Accepted	92 ms	0 KB
+### Code (python):
+
+
 a,b,x,y=map(int,input().split())
 l,r=a+b,(a+b)*5
 while l<r:
