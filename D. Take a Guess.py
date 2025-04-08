b27786ffeb9c489d6e4d6d158b5a1a7943607922
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-04-09 00:14:38 +0600

    Update D. Take a Guess.py

--- D. Take a Guess.py
+++ D. Take a Guess.py
@@ -1,3 +1,9 @@
+## Problem D - Take a Guess
+
+https://codeforces.com/contest/1556/problem/D
+290777640	Nov/10/2024 01:16UTC+6	eshat69	D - Take a Guess	Python 3	Accepted	421 ms	0 KB
+### Code (python):
+
 n, k = map(int, input().split())
 t = [0] * (n + 1)
 for i in range(1, 4):
