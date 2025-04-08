040268784b1ac3a217d09501523aa8b2196955fd
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-04-09 00:12:40 +0600

    Add files via upload

---
+++ D. Take a Guess.py
@@ -0,0 +1,9 @@
+n, k = map(int, input().split())
+t = [0] * (n + 1)
+for i in range(1, 4):
+    t[i] = int(input(f"or {i} {i%3 + 1}\n")) + int(input(f"and {i} {i%3 + 1}\n"))
+x = (t[1] + t[3] - t[2]) // 2
+t[1:4] = x, t[1] - x, t[3] - x
+for i in range(4, n + 1):
+    t[i] = int(input(f"or {1} {i}\n")) + int(input(f"and {1} {i}\n")) - x
+print("finish", sorted(t)[k])
