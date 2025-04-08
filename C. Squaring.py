a689576dd82f49249dd361929f58816c355ee066
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-04-09 00:17:35 +0600

    Update C. Squaring.py

--- C. Squaring.py
+++ C. Squaring.py
@@ -1,3 +1,9 @@
+## Problem C. Squaring
+
+https://codeforces.com/contest/1995/problem/C
+291082204	Nov/11/2024 21:25UTC+6	eshat69	C - Squaring	Python 3	Accepted	202 ms	20400 KB
+### Code (python):
+
 t = int(input())
 for j in range(t):
 	n = int(input())
