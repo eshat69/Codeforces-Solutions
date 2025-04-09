ec9f6f02880839becc275d1cbda3fa8e73169252
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-04-10 01:56:42 +0600

    Add files via upload

---
+++ 2002B - Removals Game.py
@@ -0,0 +1,5 @@
+# Assign the input function to I for shorthand usage
+I = input
+
+# Execute the code inside exec() based on the number of iterations
+exec(int(I()) * "y,h,u = (I().split() for _ in 'owo'); print(('Bob','Alice')[h != u != h[::-1]]);")
