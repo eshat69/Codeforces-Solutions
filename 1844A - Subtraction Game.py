4725789344e1460fbcf02e4f4cb508f560ba43e9
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-06-09 00:10:31 +0600

    Update 1844A - Subtraction Game.py

--- 1844A - Subtraction Game.py
+++ 1844A - Subtraction Game.py
@@ -1,1 +1,7 @@
+## Problem A. Subtraction Game
+
+https://codeforces.com/contest/1844/problem/A
+313166789	Mar/31/2025 00:08UTC+6	eshat69	A - Subtraction Game	Python 3	Accepted	62 ms	0 KB
+### Code (python):
+
 for s in[*open(0)][1:]:print(sum(map(int,s.split())))
