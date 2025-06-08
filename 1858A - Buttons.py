e1c9f1127c0537598dc3bda83167e67bb4b8557a
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-06-09 00:05:21 +0600

    Update 1858A - Buttons.py

--- 1858A - Buttons.py
+++ 1858A - Buttons.py
@@ -1,1 +1,7 @@
+## Problem A - Buttons	
+
+https://codeforces.com/contest/1858/problem/A
+313038258	Mar/30/2025 00:18UTC+6	eshat69	A - Buttons	Python 3	Accepted	77 ms	1200 KB
+### Code (python):
+
 for s in[*open(0)][1:]:a,b,c=map(int,s.split());print('SFeicrosntd'[a>b-c%2::2])
