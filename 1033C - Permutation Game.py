2d35b581f1e9993ae11a2b62d792485f21ef39a6
eshat69 <144534158+eshat69@users.noreply.github.com>
2025-08-05 00:17:51 +0600

    Update 1033C - Permutation Game.py

--- 1033C - Permutation Game.py
+++ 1033C - Permutation Game.py
@@ -1,5 +1,5 @@
 ## Problem C. Permutation Game
-
+ 
 https://codeforces.com/contest/1033/problem/C
 309865094	Mar/11/2025 00:05UTC+6	eshat69	C - Permutation Game	Python 3	Accepted	312 ms	12600 KB
 ### Code (python):
@@ -12,3 +12,4 @@ s=[0]*n
 for i,j in enumerate(b):i=n-i;s[j]='BA'[any(a[k]>a[j]and
 s[k]=='B'for k in range(j%i,n,i))]
 print(''.join(s))
+
